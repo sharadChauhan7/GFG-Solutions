f7edf22a3ec1277c018520addb68dd1daa98c792
Sharad Singh Chauhan <103198852+sharadChauhan7@users.noreply.github.com>
2025-04-09 16:45:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Bellman-Ford/bellman-ford.java
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class DriverClass {
+    public static void main(String args[]) throws IOException {
+
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String str[] = read.readLine().trim().split(" ");
+            int V = Integer.parseInt(str[0]);
+            int E = Integer.parseInt(str[1]);
+
+            // ArrayList<ArrayList<Integer>> edges = new ArrayList<>();
+            int[][] edges = new int[E][3];
+
+            int i = 0;
+            for (i = 0; i < E; i++) {
+                String S[] = read.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                int w = Integer.parseInt(S[2]);
+                // ArrayList<Integer> t1 = new ArrayList<>();
+                // t1.add(u);
+                // t1.add(v);
+                // t1.add(w);
+                // edges.add(t1);
+                edges[i][0] = u;
+                edges[i][1] = v;
+                edges[i][2] = w;
+            }
+
+            int S = Integer.parseInt(read.readLine());
+
+            Solution ob = new Solution();
+
+            int[] ptr = ob.bellmanFord(V, edges, S);
+
+            for (i = 0; i < ptr.length; i++) System.out.print(ptr[i] + " ");
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+/*   Function to implement Bellman Ford
+ *   edges: 2D array which represents the graph
+ *   src: source vertex
+ *   V: number of vertices
+ */
+class Solution {
+    static int[] bellmanFord(int V, int[][] edges, int src) {
+        // Write your code here
+        int dis[] = new int[V];
+        Arrays.fill(dis,(int)1e8);
+        dis[src]=0;
+        for(int i=0;i<V-1;i++){
+            for(int curr[]:edges){
+                if(dis[curr[0]]!=1e8 && dis[curr[0]]+curr[2]<dis[curr[1]]){
+                    dis[curr[1]]=dis[curr[0]]+curr[2];
+                }
+            }
+        }
+        // Checking for negative cycle
+        for(int curr[]:edges){
+            if(dis[curr[0]]!=1e8 && dis[curr[0]]+curr[2]<dis[curr[1]]){
+                return new int[]{-1};
+            }
+        }
+        return dis;
+        
+    }
+}
